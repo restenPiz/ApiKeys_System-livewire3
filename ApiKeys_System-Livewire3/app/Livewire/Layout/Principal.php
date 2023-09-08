697976955ef3d8a5791ff24f1ba89f88124e68bb
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-08 15:28:23 +0200

    Some updates in the application

---
+++ ApiKeys_System-Livewire3/app/Livewire/Layout/Principal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Layout;
+
+use Livewire\Component;
+
+class Principal extends Component
+{
+    public function render()
+    {
+        return view('livewire.layout.principal');
+    }
+}
