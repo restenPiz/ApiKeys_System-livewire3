d0fe2170ded9e8c7e7912ada5ff2274fda6f6b07
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-08 15:51:16 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/resources/views/livewire/login.blade.php
+++ ApiKeys_System-Livewire3/resources/views/livewire/login.blade.php
@@ -25,8 +25,8 @@
                     <div class="text-center">
                         <div class="mb-5">
                             <h1 class="display-5">Sign in</h1>
-                            <p>Don't have an account yet? <button wire:click="signUp" class="link">Sign up
-                                    here</button></p>
+                            <p>Don't have an account yet? <a href="#" wire:click="signUp" class="link">Sign up
+                                    here</a></p>
                         </div>
                     </div>
 
