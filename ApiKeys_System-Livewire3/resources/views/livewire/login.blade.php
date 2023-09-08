ec836193d38b76187b93cf062a4760b09bec2e4e
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-08 16:06:48 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/resources/views/livewire/login.blade.php
+++ ApiKeys_System-Livewire3/resources/views/livewire/login.blade.php
@@ -1,100 +1,99 @@
 <div>
-    <div class="position-fixed top-0 end-0 start-0 bg-img-start"
-        style="height: 32rem; background-image: url(assets/svg/components/card-6.svg);">
-        <!-- Shape -->
-        <div class="shape shape-bottom zi-1">
-            <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                viewBox="0 0 1921 273">
-                <polygon fill="#fff" points="0,273 1921,273 1921,0 " />
-            </svg>
-        </div>
-        <!-- End Shape -->
-    </div>
-
-    <!-- Content -->
-    <div class="container py-5 py-sm-7">
-        {{-- <a class="d-flex justify-content-center mb-5" href="index.html">
-                    <img class="zi-2" src="assets/svg/logos/logo.svg" alt="Image Description" style="width: 8rem;">
-                </a> --}}</br></br></br></br>
-
-        <div class="mx-auto" style="max-width: 30rem;">
-            <!-- Card -->
-            <div class="card card-lg mb-5">
-                <div class="card-body">
-                    <!-- Form -->
-                    <div class="text-center">
-                        <div class="mb-5">  
-                            <h1 class="display-5">Sign in</h1>
-                            <p>Don't have an account yet? <a href="#" wire:click="signUp" class="link">Sign up
-                                    here</a></p>
-                        </div>
-                    </div>
-
-                    <form class="js-validate needs-validation" novalidate>
-                        <!-- Form -->
-                        <div class="mb-4">
-                            <label class="form-label" for="signinSrEmail">Your email</label>
-                            <input type="email" class="form-control form-control-lg" name="email" id="signinSrEmail"
-                                tabindex="1" placeholder="email@address.com" aria-label="email@address.com" required>
-                            <span class="invalid-feedback">Please enter a valid email address.</span>
-                        </div>
-                        <!-- End Form -->
-
-                        <!-- Form -->
-                        <div class="mb-4">
-                            <label class="form-label w-100" for="signupSrPassword" tabindex="0">
-                                <span class="d-flex justify-content-between align-items-center">
-                                    <span>Password</span>
-                                    <a class="form-label-link mb-0"
-                                        href="authentication-reset-password-basic.html">Forgot Password?</a>
-                                </span>
-                            </label>
-
-                            <div class="input-group input-group-merge" data-hs-validation-validate-class>
-                                <input type="password" class="js-toggle-password form-control form-control-lg"
-                                    name="password" id="signupSrPassword" placeholder="8+ characters required"
-                                    aria-label="8+ characters required" required minlength="8"
-                                    data-hs-toggle-password-options='{
-                           "target": "#changePassTarget",
-                           "defaultClass": "bi-eye-slash",
-                           "showClass": "bi-eye",
-                           "classChangeTarget": "#changePassIcon"
-                         }'>
-                                <a id="changePassTarget" class="input-group-append input-group-text"
-                                    href="javascript:;">
-                                    <i id="changePassIcon" class="bi-eye"></i>
-                                </a>
-                            </div>
-
-                            <span class="invalid-feedback">Please enter a valid password.</span>
-                        </div>
-                        <!-- End Form -->
-
-                        <!-- Form Check -->
-                        <div class="form-check mb-4">
-                            <input class="form-check-input" type="checkbox" value="" id="termsCheckbox">
-                            <label class="form-check-label" for="termsCheckbox">
-                                Remember me
-                            </label>
-                        </div>
-                        <!-- End Form Check -->
-
-                        <div class="d-grid">
-                            <button type="submit" class="btn btn-primary btn-lg">Sign in</button>
-                        </div>
-                    </form>
-                    <!-- End Form -->
-                </div>
-            </div>
-            <!-- End Card -->
-
-        </div>
-    </div>
-
-    {{--Inicio do metodo de redirecionamento--}}
-
     @if($redirectToSignUp)
         @livewire('signUp')
+    @else
+        <div class="position-fixed top-0 end-0 start-0 bg-img-start"
+            style="height: 32rem; background-image: url(assets/svg/components/card-6.svg);">
+            <!-- Shape -->
+            <div class="shape shape-bottom zi-1">
+                <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                    viewBox="0 0 1921 273">
+                    <polygon fill="#fff" points="0,273 1921,273 1921,0 " />
+                </svg>
+            </div>
+            <!-- End Shape -->
+        </div>
+
+        <!-- Content -->
+        <div class="container py-5 py-sm-7">
+            {{-- <a class="d-flex justify-content-center mb-5" href="index.html">
+                        <img class="zi-2" src="assets/svg/logos/logo.svg" alt="Image Description" style="width: 8rem;">
+                    </a> --}}</br></br></br></br>
+
+            <div class="mx-auto" style="max-width: 30rem;">
+                <!-- Card -->
+                <div class="card card-lg mb-5">
+                    <div class="card-body">
+                        <!-- Form -->
+                        <div class="text-center">
+                            <div class="mb-5">  
+                                <h1 class="display-5">Sign in</h1>
+                                <p>Don't have an account yet? <a href="#" wire:click="signUp" class="link">Sign up
+                                        here</a></p>
+                            </div>
+                        </div>
+
+                        <form class="js-validate needs-validation" novalidate>
+                            <!-- Form -->
+                            <div class="mb-4">
+                                <label class="form-label" for="signinSrEmail">Your email</label>
+                                <input type="email" class="form-control form-control-lg" name="email" id="signinSrEmail"
+                                    tabindex="1" placeholder="email@address.com" aria-label="email@address.com" required>
+                                <span class="invalid-feedback">Please enter a valid email address.</span>
+                            </div>
+                            <!-- End Form -->
+
+                            <!-- Form -->
+                            <div class="mb-4">
+                                <label class="form-label w-100" for="signupSrPassword" tabindex="0">
+                                    <span class="d-flex justify-content-between align-items-center">
+                                        <span>Password</span>
+                                        <a class="form-label-link mb-0"
+                                            href="authentication-reset-password-basic.html">Forgot Password?</a>
+                                    </span>
+                                </label>
+
+                                <div class="input-group input-group-merge" data-hs-validation-validate-class>
+                                    <input type="password" class="js-toggle-password form-control form-control-lg"
+                                        name="password" id="signupSrPassword" placeholder="8+ characters required"
+                                        aria-label="8+ characters required" required minlength="8"
+                                        data-hs-toggle-password-options='{
+                            "target": "#changePassTarget",
+                            "defaultClass": "bi-eye-slash",
+                            "showClass": "bi-eye",
+                            "classChangeTarget": "#changePassIcon"
+                            }'>
+                                    <a id="changePassTarget" class="input-group-append input-group-text"
+                                        href="javascript:;">
+                                        <i id="changePassIcon" class="bi-eye"></i>
+                                    </a>
+                                </div>
+
+                                <span class="invalid-feedback">Please enter a valid password.</span>
+                            </div>
+                            <!-- End Form -->
+
+                            <!-- Form Check -->
+                            <div class="form-check mb-4">
+                                <input class="form-check-input" type="checkbox" value="" id="termsCheckbox">
+                                <label class="form-check-label" for="termsCheckbox">
+                                    Remember me
+                                </label>
+                            </div>
+                            <!-- End Form Check -->
+
+                            <div class="d-grid">
+                                <button type="submit" class="btn btn-primary btn-lg">Sign in</button>
+                            </div>
+                        </form>
+                        <!-- End Form -->
+                    </div>
+                </div>
+                <!-- End Card -->
+
+            </div>
+        </div>
+
     @endif
 
     {{--Fim do metodo de redirecionamento--}}
