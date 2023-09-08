e29a633615112c992566437873081184c0acbc02
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-08 15:38:22 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/resources/views/livewire/layout/principal.blade.php
+++ ApiKeys_System-Livewire3/resources/views/livewire/layout/principal.blade.php
@@ -6,11 +6,67 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Principal</title>
 
+    {{--Inicio dos links do css--}}
+
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <!-- Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&amp;display=swap" rel="stylesheet">
+
+    <!-- CSS Implementing Plugins -->
+    <link rel="stylesheet" href="assets/css/vendor.min.css">
+
+    <!-- CSS Front Template -->
+    <link rel="stylesheet" href="assets/css/theme.minc619.css?v=1.0">
+
+    <link rel="preload" href="assets/css/theme.min.css" data-hs-appearance="default" as="style">
+    <link rel="preload" href="assets/css/theme-dark.min.css" data-hs-appearance="dark" as="style">
+
+    <style data-hs-appearance-onload-styles>
+        *
+        {
+        transition: unset !important;
+        }
+
+        body
+        {
+        opacity: 0;
+        }
+    </style>
     @livewireStyles
 </head>
 <body>
     
     {{$slot}}
+
+
+    {{--Inicio dos links do javascript--}}
+        
+    <script src="assets/js/vendor.min.js"></script>
+
+    <!-- JS Front -->
+    <script src="assets/js/theme.min.js"></script>
+
+    <!-- JS Plugins Init. -->
+    <script>
+        (function() {
+        window.onload = function () {
+            // INITIALIZATION OF BOOTSTRAP VALIDATION
+            // =======================================================
+            HSBsValidation.init('.js-validate', {
+            onSubmit: data => {
+                data.event.preventDefault()
+                alert('Submited')
+            }
+            })
+
+
+            // INITIALIZATION OF TOGGLE PASSWORD
+            // =======================================================
+            new HSTogglePassword('.js-toggle-password')
+        }
+        })()
+    </script>
 
     @livewireScripts
 
