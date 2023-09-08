3a6ec4e6895a8651d1d864d5ec33e819c297f646
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-09 00:52:23 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/app/Livewire/Signup.php
+++ ApiKeys_System-Livewire3/app/Livewire/Signup.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
