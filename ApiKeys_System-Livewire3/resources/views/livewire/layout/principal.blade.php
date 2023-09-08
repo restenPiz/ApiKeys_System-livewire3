cc94a149400dabbc2b60e785390d29d930e7c9fd
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-08 15:30:58 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/resources/views/livewire/layout/principal.blade.php
+++ ApiKeys_System-Livewire3/resources/views/livewire/layout/principal.blade.php
@@ -5,8 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Principal</title>
+
+    @livewireStyles
 </head>
 <body>
     
+    {{$slot}}
+
+    @livewireScripts
+
 </body>
 </html>
