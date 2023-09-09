2b20d7ec55ca054595f8c6cbe73a492a0c2d5ccd
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-09 02:05:38 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/resources/views/layouts/principal.blade.php
+++ ApiKeys_System-Livewire3/resources/views/layouts/principal.blade.php
@@ -215,24 +215,17 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100">
-        {{-- @include('layouts.navigation') --}}
+<body class="has-navbar-vertical-aside navbar-vertical-aside-show-xl   footer-offset">
 
-        <!-- Page Heading -->
-        @if (isset($header))
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endif
-
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
-    </div>
+  
+    <!-- ========== HEADER ========== -->
+  
+    @include('layouts.navigation')
+    
+    <main id="content" role="main" class="main">
+                 {{ $slot }}
+    </main>
+    
 
     {{-- Inicio dos links do javascript --}}
 
@@ -391,6 +384,9 @@
         })()
     </script>
 
+    <script src="assets/js/hs.theme-appearance.js"></script>
+    
+    <script src="assets/vendor/hs-navbar-vertical-aside/dist/hs-navbar-vertical-aside-mini-cache.js"></script>
     @livewireScripts
 
 </body>
