15761a2556cb9a35fd59d544f8f0b67046b8c40a
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-10 23:28:20 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/app/Livewire/Dashboard.php
+++ ApiKeys_System-Livewire3/app/Livewire/Dashboard.php
@@ -8,6 +8,7 @@ class Dashboard extends Component
 {
     public function render()
     {
+        ///?incio do metodo que retorna a dashboard
         return view('livewire.dashboard')->layout('layouts.principal');
     }
 }
