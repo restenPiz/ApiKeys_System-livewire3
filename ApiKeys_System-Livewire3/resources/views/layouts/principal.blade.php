33cb4e73cc59cda402a3bb5e94d609c62d9fe0e4
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-11 20:40:51 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/resources/views/layouts/principal.blade.php
+++ ApiKeys_System-Livewire3/resources/views/layouts/principal.blade.php
@@ -216,7 +216,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="has-navbar-vertical-aside navbar-vertical-aside-show-xl   footer-offset">
+<body>
   
     {{ $slot }}
 
