96a5318c732f604a3950b8dd5afe675ece1472c8
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-12 22:48:08 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/app/Livewire/Dashboard.php
+++ ApiKeys_System-Livewire3/app/Livewire/Dashboard.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire;
 
+use App\Models\ApiKeys;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
     public function render()
     {
-        return view('livewire.dashboard')->layout('layouts.principal');
+        $keys=ApiKeys::all();
+
+        return view('livewire.dashboard',compact('keys'))->layout('layouts.principal');
     }
 }
