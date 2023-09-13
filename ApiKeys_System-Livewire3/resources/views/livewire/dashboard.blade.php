9f7bf99e4c48f68a7194c7b745337058bb7a60e2
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-14 00:31:29 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/resources/views/livewire/dashboard.blade.php
+++ ApiKeys_System-Livewire3/resources/views/livewire/dashboard.blade.php
@@ -122,7 +122,8 @@
                 <div class="mb-2 mb-sm-0">
                     <h4 class="card-header-title">API Keys</h4>
                     <p class="card-text fs-5">Keys you have generated to connect with third-party clients or access the
-                        <a class="link" href="#">Front API.</a></p>
+                        <a class="link" href="#">Front API.</a>
+                    </p>
                 </div>
 
                 <div class="d-grid d-sm-flex justify-content-sm-between align-items-sm-center gap-2">
@@ -331,5 +332,58 @@
         <!-- End Card -->
     </div>
     <!-- End Content -->
+
+    {{-- Inicio do modal de adicionar --}}
+
+    <!-- Create New API Key Modal -->
+    <div class="modal fade" id="createAKIKeyModal" tabindex="-1" aria-labelledby="createAKIKeyModalLabel"
+        role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <!-- Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title" id="createAKIKeyModalLabel">Create API Key</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <!-- End Header -->
+
+                <!-- Body -->
+                <div class="modal-body">
+                    <!-- Form -->
+                    <form>
+                        <input type="text" class="form-control" placeholder="API Key name">
+                    </form>
+                    <!-- End Form -->
+                </div>
+                <!-- End Body -->
+
+                <!-- Footer -->
+                <div class="modal-footer">
+                    <div class="row align-items-sm-center flex-grow-1 mx-n2">
+                        <div class="col-sm mb-2 mb-sm-0">
+                            <p class="modal-footer-text">What is an API? <i class="bi-question-circle"
+                                    data-bs-toggle="tooltip" data-bs-placement="top"
+                                    title="API stands for application programming interface. It can be helpful to think of the API as a way for different apps to talk to one another."></i>
+                            </p>
+                        </div>
+                        <!-- End Col -->
+
+                        <div class="col-sm-auto">
+                            <div class="d-flex gap-3">
+                                <button type="button" class="btn btn-white" data-bs-dismiss="modal"
+                                    aria-label="Close">Cancel</button>
+                                <button type="button" class="btn btn-primary">Generate</button>
+                            </div>
+                        </div>
+                        <!-- End Col -->
+                    </div>
+                    <!-- End Row -->
+                </div>
+                <!-- End Footer -->
+            </div>
+        </div>
+    </div>
+
+    {{-- Fim do modal de adicionar --}}
 
 </div>
