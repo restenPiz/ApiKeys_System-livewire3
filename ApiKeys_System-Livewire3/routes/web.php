e3e1779ba9540bdb2f4bbf95ec613d24f56674f7
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-13 22:41:41 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/routes/web.php
+++ ApiKeys_System-Livewire3/routes/web.php
@@ -9,6 +9,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [authController::class, 'login'])->name('login');
 Route::get('/signOut', [authController::class, 'register'])->name('sign');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
