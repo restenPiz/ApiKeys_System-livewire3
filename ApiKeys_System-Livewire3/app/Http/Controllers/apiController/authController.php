562f26d59211c245ac7e430af44b1a0bb2cba6b5
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-13 22:04:26 +0200

    Some updates in the application

---
+++ ApiKeys_System-Livewire3/app/Http/Controllers/apiController/authController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\apiController;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class authController extends Controller
+{
+    public function login()
+    {
+        return view('auth.login');
+    }
+}
--- ApiKeys_System-Livewire3/routes/web.php
+++ ApiKeys_System-Livewire3/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\apiController\authController;
 use App\Http\Controllers\ProfileController;
 use App\Livewire\Login;
 use App\Livewire\Signup;
@@ -7,9 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 //!Inicio das rotas do meu sistema
 
-Route::get('/',Login::class)->name('login');
-Route::get('/signUp',Signup::class)->name('signUp');
-
+Route::get('/', [authController::class, 'login'])->name('login');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
