cce282e0a7055d2badb1b433a7392687db75aa51
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-14 00:10:23 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/routes/web.php
+++ ApiKeys_System-Livewire3/routes/web.php
@@ -10,12 +10,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [authController::class, 'login'])->name('login');
 Route::get('/signOut', [authController::class, 'register'])->name('sign');
 
+//?Rota responsavel por abrir o dashboard que e um componente livewire
+
 Route::get('/dashboard', Dashboard::class);
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 require __DIR__.'/auth.php';
