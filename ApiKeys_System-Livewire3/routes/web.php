dc5deb6a32a94f6f5ccb8ca3c88093f703f4100d
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-14 00:14:11 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/routes/web.php
+++ ApiKeys_System-Livewire3/routes/web.php
@@ -14,10 +14,10 @@ Route::get('/signOut', [authController::class, 'register'])->name('sign');
 
 Route::get('/dashboard', Dashboard::class);
 
+require __DIR__.'/auth.php';
+
 // Route::middleware('auth')->group(function () {
 //     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 //     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
 //     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 // });
-
-require __DIR__.'/auth.php';
