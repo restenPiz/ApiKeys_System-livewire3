887abf0db0aabd734f38a4d3091a3e7d3d6ce5a7
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-13 22:45:09 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/resources/views/auth/register.blade.php
+++ ApiKeys_System-Livewire3/resources/views/auth/register.blade.php
@@ -34,7 +34,7 @@
                             </div>
                         </div>
 
-                        <form class="js-validate needs-validation" action="" method="POST" novalidate>
+                        <form class="js-validate needs-validation" action="{{route('register')}}" method="POST" novalidate>
                             <!-- Form -->
                             <div class="mb-4">
                                 <label class="form-label" for="signinSrEmail">Your name</label>
