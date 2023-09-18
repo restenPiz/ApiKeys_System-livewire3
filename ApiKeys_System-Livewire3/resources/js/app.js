8bf8ae999db45dddf4ba0d9212eaefbea35a3f95
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-18 14:06:12 +0200

    Some updates in the application

--- ApiKeys_System-Livewire3/resources/js/app.js
+++ ApiKeys_System-Livewire3/resources/js/app.js
@@ -5,3 +5,19 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// resources/js/app.js
+
+require('alpinejs');
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Verifica se há um alerta flash na página
+    const alert = document.querySelector('.alert');
+    
+    if (alert) {
+        // Fecha o alerta após 5 segundos
+        setTimeout(function () {
+            alert.style.display = 'none';
+        }, 5000);
+    }
+});
